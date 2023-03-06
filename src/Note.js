76d73bc5e85ddd55da0eaeb9e88299dbc4af611a
Mason <mason.bullock@ucalgary.ca>
2023-03-06 05:41:07 -0700

    notetaking area formatted

--- src/Note.js
+++ src/Note.js
@@ -1,7 +1,38 @@
 function NotePane(){
     return(
         <div id="note-space">
-            <p>Select a note, or create a new one.</p>
+            <div id="empty-pane">
+                <p>Select a note, or create a new one.</p>
+            </div>
+            <div id="active-pane">
+                <div id="note-header-bar">
+                    <div id="note-header-pane-container">
+                        <input type="text" class="note-title" autoFocus></input>
+                        <div id="note-date">
+                            <p id="date-time">YYYY-MM-DD, HH:MM:SS PM</p>
+                            <i class="fa-regular fa-calendar"></i>
+                        </div>
+                    </div>
+                    <p class="clickable">Save</p>
+                    <p class="clickable">Delete</p>
+                </div>
+                <div id="tool-bar">
+                    <select>
+                        <option>Normal</option>
+                        <option>font 1</option>
+                        <option>font 2</option>
+                    </select>
+                    <i class="fa-sharp fa-solid fa-bold"></i>
+                    <i class="fa-sharp fa-solid fa-italic"></i>
+                    <i class="fa-sharp fa-solid fa-underline"></i>
+                    <i class="fa-sharp fa-solid fa-link"></i>
+                    <i class="fa-sharp fa-solid fa-list"></i>
+                    <i class="fa-sharp fa-solid fa-list-ol"></i>
+                    <i class="fa-sharp fa-solid fa-text-slash"></i>
+                </div>
+                <textarea></textarea>
+            </div>
+            
         </div>
     );
 }
--- src/Tab.js
+++ src/Tab.js
@@ -5,8 +5,12 @@ function MenuTab(){
                 <p id="tab-title">Notes</p>
                 <p id="add-btn">+</p>
             </div>
-            <div>
-                test
+            <div id="tab-notes">
+                <div class="tab-note">
+                    <h6>TITLE</h6>
+                    <p class="date">DATE -- DD/MM/YYYY</p>
+                    <p class="preview">NOTE PREVIEW</p>
+                </div> 
             </div>
         </div>
     );
