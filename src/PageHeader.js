aeb34596746b2dbfb19316eef04316572e51b2ea
Mason <mason.bullock@ucalgary.ca>
2023-03-08 12:59:11 -0700

    implemented the toggle menu button

--- src/PageHeader.js
+++ src/PageHeader.js
@@ -1,7 +1,20 @@
 function Header(){
+
+    const toggleMenu = () => {
+        console.log("test");
+        if(document.getElementById("tab").style.display === "none"){
+            document.getElementById("tab").style.display = "block";
+            
+        }else{
+            document.getElementById("tab").style.display = "none";
+            document.getElementById("note-space").style.margin = "auto";
+        }
+        
+    };
+
     return(
         <header>
-      <div id="menu">{"\u2630"}</div>
+      <div id="menu" onClick={()=>toggleMenu()}>{"\u2630"}</div>
       <div id="title">
         <h1>Lotion</h1>
         <p>Like Notion But Worse</p>
